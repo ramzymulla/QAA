e917b5072c9a16ae4f721e3e536d2d90599bc951
Ramzy Al-Mulla <rza@n0349.talapas.uoregon.edu>
2024-09-10 23:16:41 -0700

    done?

---
+++ scripts/htseq22-3H_uns.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH -A bgmp
+#SBATCH -p bgmp
+#SBATCH -t0-20
+#SBATCH --output=logs/htseq22-3H_live_%j.out
+#SBATCH --error=logs/htseq22-3H_live_%j.err
+#SBATCH --mail-user=rza@uoregon.edu
+#SBATCH --mail-type=ALL
+
+conda activate QAA
+
+
+wd="/projects/bgmp/rza/bioinfo/Bi623/"
+file="QAA/aligned/22_3H_both_S16_L008Aligned.out.sam"
+gtf="mousedata/Mus_musculus.GRCm39.112.gtf"
+
+/usr/bin/time -v htseq-count --stranded=no ${wd}$file ${wd}$gtf > htseq22-3H_uns.txt
